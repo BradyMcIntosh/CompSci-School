e03b2ab5368a48a81d7795c6dd8941ce9516b0e6
Brady John McIntosh <36173519+BradyMcIntosh@users.noreply.github.com>
2020-02-12 03:12:56 -0500

    Upload CST8130 Hybrid 3

---
+++ Semester 3/CST8130 Data Structures/CST8130 Hybrid 3/src/Hybrid3.java
@@ -0,0 +1,21 @@
+
+public class Hybrid3 {
+
+	public static void main(String[] args) {
+		
+		int n = 17;
+		for(int i = 1; i < n; i*=2) {
+			
+			int j = n;
+			while (n>0) {
+				System.out.print(" " + i + " " + j);
+				j--;
+			}
+			System.out.println();
+		}
+		
+		
+	}
+	
+	
+}
